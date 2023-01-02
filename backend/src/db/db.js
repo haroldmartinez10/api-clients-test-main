db71b4e102e4ce1d626a98ce9c2b038f16677592
Harold Martinez <100185742+haroldmartinez10@users.noreply.github.com>
2023-01-02 18:56:46 -0500

    Update db.js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -18,17 +18,6 @@ const connectDB = async () => {
     process.exit(1)
   }
 
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('Mongo DB Conectado!')
-
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
 }
 
 export default connectDB
